b937c24c240bbc4208b49462f8c23b8a0cd7cd24
Ricardo Caro <ricardo.caro@growdata.com.co>
2021-11-11 16:12:44 -0500

    Controladores y acciones

---
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeliculasAPI.Entidades;
+using PeliculasAPI.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Controllers
+{
+    [Route("api/generos")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        private readonly IRepositorio repositorio;
+
+        public GenerosController(IRepositorio repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        [HttpGet]
+        public List<Genero> Get()
+        {
+            return repositorio.ObtenerTodosLosGeneros();
+        }
+
+        [HttpGet]
+        public Genero Get(int Id)
+        {
+            var genero = repositorio.ObtenerPorId(Id);
+            if (genero == null)
+            {
+                //NotFound();
+            }
+            return genero;
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
