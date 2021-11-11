c24e4b51e2a370f6862c620a7fd99612f7b31dcf
Ricardo Caro <ricardo.caro@growdata.com.co>
2021-11-11 15:00:32 -0500

    Agregar archivos de proyecto.

---
+++ PeliculasAPI/Repositorios/RepositorioEnMemoria.cs
@@ -0,0 +1,25 @@
+﻿using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Repositorios
+{
+    public class RepositorioEnMemoria
+    {
+        private List<Genero> _generos;
+        public RepositorioEnMemoria()
+        {
+            _generos = new List<Genero>() {
+                new Genero(){ Id = 1, Nombre="Comedia"},
+                new Genero(){ Id = 1, Nombre="Accion"}
+            };
+        }
+
+        public List<Genero> ObtenerTodosLosGeneros()
+        {
+            return _generos;
+        }
+    }
+}
